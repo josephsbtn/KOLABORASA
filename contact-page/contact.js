4cc4de84e8feda1b2e6e93f7263ff1beee1a0648
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-02-26 20:26:02 +0700

    walawe

--- contact-page/contact.js
+++ contact-page/contact.js
@@ -1,6 +1,8 @@
-const navbarnav = document.querySelector(".navbar-nav");
-const menu = document.querySelector("menu");
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarnav = document.querySelector(".navbar-nav");
+  const menu = document.getElementById("menu");
 
-menu.onclick = () => {
-  navbarnav.classList.toggle("active");
-};
+  menu.onclick = () => {
+    navbarnav.classList.toggle("active");
+  };
+});
